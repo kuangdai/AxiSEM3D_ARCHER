0d4b8194e7bac9d3402032bcf1a3f2a6785de69a
kuangdai <kuangdai.leng@earth.ox.ac.uk>
2016-10-12 23:15:12 +0100

    subdri

--- gen.sh
+++ gen.sh
@@ -2,7 +2,8 @@ export T0=10
 export model=prem_ani
 export nproc=24
 export wtime="1:0:0"
-export jname=xx
+export subdir="ellip"
+export jname=pr2_10_off
 
 if [ $((${nproc}%24)) -gt 0 ]; then
     export nnode=$((${nproc}/24+1))
@@ -10,12 +11,16 @@ else
     export nnode=$((${nproc}/24))    
 fi
 
-rsync -r template/ ../AxiSEM3D_RUNS/${jname}/
-perl -pi -w -e "s/__t0__/${T0}/g;"            ../AxiSEM3D_RUNS/${jname}/input/CMTSOLUTION
-perl -pi -w -e "s/__t0__/${T0}/g;"            ../AxiSEM3D_RUNS/${jname}/input/inparam.basic
-perl -pi -w -e "s/__model__/${model}/g;"      ../AxiSEM3D_RUNS/${jname}/input/inparam.basic
-perl -pi -w -e "s/__NPROC__/${nproc}/g;"      ../AxiSEM3D_RUNS/${jname}/axi.bolt
-perl -pi -w -e "s/__NNODE__/${nnode}/g;"      ../AxiSEM3D_RUNS/${jname}/axi.bolt
-perl -pi -w -e "s/__JOBNAME__/${jname}/g;"    ../AxiSEM3D_RUNS/${jname}/axi.bolt
-perl -pi -w -e "s/__WALLTIME__/${wtime}/g;"   ../AxiSEM3D_RUNS/${jname}/axi.bolt
+mkdir ../AxiSEM3D_RUNS/${subdir}
+mkdir ../AxiSEM3D_RUNS/${subdir}/${jname}
+rsync -r --force --delete template/ ../AxiSEM3D_RUNS/${subdir}/${jname}/
+
+perl -pi -w -e "s/__t0__/${T0}/g;"            ../AxiSEM3D_RUNS/${subdir}/${jname}/input/CMTSOLUTION
+perl -pi -w -e "s/__t0__/${T0}/g;"            ../AxiSEM3D_RUNS/${subdir}/${jname}/input/inparam.basic
+perl -pi -w -e "s/__model__/${model}/g;"      ../AxiSEM3D_RUNS/${subdir}/${jname}/input/inparam.basic
+perl -pi -w -e "s/__NPROC__/${nproc}/g;"      ../AxiSEM3D_RUNS/${subdir}/${jname}/axi.bolt
+perl -pi -w -e "s/__NNODE__/${nnode}/g;"      ../AxiSEM3D_RUNS/${subdir}/${jname}/axi.bolt
+perl -pi -w -e "s/__JOBNAME__/${jname}/g;"    ../AxiSEM3D_RUNS/${subdir}/${jname}/axi.bolt
+perl -pi -w -e "s/__SUBDIR__/${subdir}/g;"    ../AxiSEM3D_RUNS/${subdir}/${jname}/axi.bolt
+perl -pi -w -e "s/__WALLTIME__/${wtime}/g;"   ../AxiSEM3D_RUNS/${subdir}/${jname}/axi.bolt
 
