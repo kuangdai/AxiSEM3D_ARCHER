2b35040dd914b1b550232d05e81b1ac14fd4fc5f
kuangdai <kuangdai.leng@earth.ox.ac.uk>
2016-10-13 23:33:30 +0100

    OFAST

--- cmake.sh
+++ cmake.sh
@@ -5,7 +5,7 @@ cp CMakeLists.txt ../AxiSEM3D/SOLVER/CMakeLists.txt
 rm -rf build
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=release ../../AxiSEM3D/SOLVER
+cmake -DCMAKE_BUILD_TYPE=RELEASE ../../AxiSEM3D/SOLVER
 make -j16
 cp axisem3d ../template/
 cd .. 
