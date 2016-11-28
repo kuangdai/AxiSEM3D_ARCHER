91ccaaea4bcc77233b9bda3c945e4555022950dd
kuangdai <kuangdai.leng@earth.ox.ac.uk>
2016-11-28 18:33:58 +0000

    reform

--- gen.sh
+++ gen.sh
@@ -1,22 +1,43 @@
-export T0=10
-export model=prem_ani
+# model
+export model=prem_ani_spherical_2D_10s.e
+export att_spec=true
+# source
+export cmt=CMTSOLUTION_XJCh_GCMT
+export station=STATIONS_XJCh
+# simulation
 export nproc=24
 export wtime="0:30:0"
 export subdir="ellip"
 export jname=pr2_10_geo
 
+
+# copy
+mkdir ../AxiSEM3D_RUNS/${subdir}
+mkdir ../AxiSEM3D_RUNS/${subdir}/${jname}
+rsync -r --force --delete template/ ../AxiSEM3D_RUNS/${subdir}/${jname}/
+
+# model
+perl -pi -w -e "s/__model__/${model}/g;"      ../AxiSEM3D_RUNS/${subdir}/${jname}/input/inparam.model
+if [ ${att_spec} == true ]; then
+    perl -pi -w -e "s/__att_specfem__/true/g;"      ../AxiSEM3D_RUNS/${subdir}/${jname}/input/inparam.advanced
+    perl -pi -w -e "s/__att_kappa__/false/g;"      ../AxiSEM3D_RUNS/${subdir}/${jname}/input/inparam.advanced
+    cp template/meshes_nr3/${model} ../AxiSEM3D_RUNS/${subdir}/${jname}/input/
+else
+    perl -pi -w -e "s/__att_specfem__/false/g;"      ../AxiSEM3D_RUNS/${subdir}/${jname}/input/inparam.advanced
+    perl -pi -w -e "s/__att_kappa__/true/g;"      ../AxiSEM3D_RUNS/${subdir}/${jname}/input/inparam.advanced
+    cp template/meshes_nr5/${model} ../AxiSEM3D_RUNS/${subdir}/${jname}/input/
+fi
+
+# source
+cp template/cmt_sta/${cmt} ../AxiSEM3D_RUNS/${subdir}/${jname}/input/CMTSOLUTION
+cp template/cmt_sta/${station} ../AxiSEM3D_RUNS/${subdir}/${jname}/input/STATIONS
+
+# simulation
 if [ $((${nproc}%24)) -gt 0 ]; then
     export nnode=$((${nproc}/24+1))
 else
     export nnode=$((${nproc}/24))    
 fi
-
-mkdir ../AxiSEM3D_RUNS/${subdir}
-mkdir ../AxiSEM3D_RUNS/${subdir}/${jname}
-rsync -r --force --delete template/ ../AxiSEM3D_RUNS/${subdir}/${jname}/
-
-perl -pi -w -e "s/__t0__/${T0}/g;"            ../AxiSEM3D_RUNS/${subdir}/${jname}/input/inparam.basic
-perl -pi -w -e "s/__model__/${model}/g;"      ../AxiSEM3D_RUNS/${subdir}/${jname}/input/inparam.basic
 perl -pi -w -e "s/__NPROC__/${nproc}/g;"      ../AxiSEM3D_RUNS/${subdir}/${jname}/axi.bolt
 perl -pi -w -e "s/__NNODE__/${nnode}/g;"      ../AxiSEM3D_RUNS/${subdir}/${jname}/axi.bolt
 perl -pi -w -e "s/__JOBNAME__/${jname}/g;"    ../AxiSEM3D_RUNS/${subdir}/${jname}/axi.bolt
